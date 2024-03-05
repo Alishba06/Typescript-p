68ea7a47ad7127eb4b6d1339354b5c31912da5fc
A06 <160205029+Alishba06@users.noreply.github.com>
2024-03-05 16:37:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// let name1:string="alishba"
+// console.log("alishba")
+// let name2:string="javed"
+// console.log("javed")
+// let name3:string="meo"
+// console.log("meo")
+var name4 = "rajput";
+console.log("rajput");
---
+++ index.ts
@@ -0,0 +1,15 @@
+let name1:string="alishba"
+
+console.log("alishba")
+
+let name2:string="javed"
+
+console.log("javed")
+
+let name3:string="meo"
+
+console.log("meo")
+
+let name4:string="rajput"
+
+console.log("rajput")
